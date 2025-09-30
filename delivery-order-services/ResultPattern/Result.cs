fa47e0cb0dcd0c8259ddbb60fce7809d2400e29c
Otavbio Augusto Reis Almeida <oaugusto265@gmail.com>
2025-09-29 22:24:06 -0300

    feat: Subindo result Pattern

---
+++ delivery-order-services/ResultPattern/Result.cs
@@ -0,0 +1,24 @@
+﻿namespace delivery_order_services.ResultPattern
+{
+    public class Result<T> where T: class
+    {
+        public T Content { get; set; }
+        public string ErrorMessage { get; set; }
+
+        public Result(string errorMesssage)
+        {
+            if(string.IsNullOrEmpty(errorMesssage)) 
+                ErrorMessage = "Ocorreu um erro inesperado";
+                
+            ErrorMessage = errorMesssage;
+        }
+
+        public Result()
+        {
+        }
+
+        public Result<T> Success(T result) => new();
+        public Result<T> Failed(string? errorMessage) => new(errorMessage);
+    }
+
+}
