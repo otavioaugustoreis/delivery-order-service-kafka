72ba4d972b595607cf6e29dbaa2669921ac16906
Otavbio Augusto Reis Almeida <oaugusto265@gmail.com>
2026-04-23 22:12:42 -0300

    feat: adicionando consumer

---
+++ delivery-order-services.Notify/Features/OrderCreatedConsumer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delivery_order_services.Notify.Features
+{
+    public class OrderCreatedConsumer
+    {
+    }
+}
