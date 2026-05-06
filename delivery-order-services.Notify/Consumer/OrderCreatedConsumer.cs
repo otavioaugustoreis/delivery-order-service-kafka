5e2d86cd567fe5d52eeaf3f79c68172360382f5e
Otavbio Augusto Reis Almeida <oaugusto265@gmail.com>
2026-05-05 21:59:09 -0300

    fix: ajustando nomes das pastas

--- delivery-order-services.Notify/Consumer/OrderCreatedConsumer.cs
+++ delivery-order-services.Notify/Consumer/OrderCreatedConsumer.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace delivery_order_services.Notify.Features
+﻿namespace delivery_order_services.Notify.Features
 {
     public class OrderCreatedConsumer
     {
+        private readonly ILogger<OrderCreatedConsumer> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly string _orderTopicName;
+        private readonly string _kafkaBootstrapServers;
+        private readonly string _notifierConsumeGroupName; 
     }
 }
